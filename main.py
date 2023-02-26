890750222306941608a74ff2ada5aeb2d2f0787b
juliandp30 <j.parra@rdc-consultores.com>
2023-02-26 18:01:56 -0500

    form

--- main.py
+++ main.py
@@ -19,7 +19,14 @@ if data is not None:
         "Área del proyecto (m²)", value=10000, min_value=1
     )
 
+import streamlit as st
 
+form = st.form("my_form")
+form.slider("Inside the form")
+st.slider("Outside the form")
+
+# Now add a submit button to the form:
+form.form_submit_button("Submit")
 
 
 # data_heads = data.columns.values
