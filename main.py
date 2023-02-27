ff4e81756dc57084ad1033dbf9b89d4068c01d3d
juliandp30 <j.parra@rdc-consultores.com>
2023-02-27 12:20:55 -0500

    2d graph

--- main.py
+++ main.py
@@ -77,3 +77,18 @@ if submitted1:
     fig.update_yaxes(title="Puntaje", visible=True, showticklabels=False)
 
     st.plotly_chart(fig, use_container_width=True)
+
+
+    st.title("Peso total del refuerzo")
+
+    fig = px.scatter(
+        x = list_gh['keys'],
+        y = list_gh['by_weigth'],
+        labels={"value": "", "variable": "Indicador"},
+        height=700,
+    )
+    fig.update_layout(font_size=20, plot_bgcolor='rgba(180, 180, 180, 0.3)')
+    fig.update_xaxes(title="Opciones de refuerzo", visible=True, showticklabels=False)
+    fig.update_yaxes(title="Peso total", visible=True, showticklabels=False)
+
+    st.plotly_chart(fig, use_container_width=True)
