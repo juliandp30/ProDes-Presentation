9e8fa64efdfa31b543a46286ab408d9ec05bb3b5
juliandp30 <j.parra@rdc-consultores.com>
2023-05-16 11:33:53 -0500

    x values for puntaje vs precio

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ if submitted1:
         height=700,
     )
     fig.update_layout(font_size=20, plot_bgcolor='rgba(180, 180, 180, 0.3)')
-    fig.update_xaxes(title="Precio", visible=True, showticklabels=False)
+    fig.update_xaxes(title="Precio", visible=True, showticklabels=True)
     fig.update_yaxes(title="Puntaje", visible=True, showticklabels=True)
 
     st.plotly_chart(fig, use_container_width=True)
