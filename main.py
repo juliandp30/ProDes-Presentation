9898130ebd92d8b60f1400b5e851043763efa075
juliandp30 <j.parra@rdc-consultores.com>
2023-05-16 11:36:30 -0500

    dtick

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ if submitted1:
     )
     fig.update_layout(font_size=20, plot_bgcolor='rgba(180, 180, 180, 0.3)')
     fig.update_xaxes(title="Opciones de refuerzo", visible=True, showticklabels=False)
-    fig.update_yaxes(title="Puntaje", visible=True, showticklabels=True)
+    fig.update_yaxes(title="Puntaje", visible=True, showticklabels=True, dtick=20)
 
     st.plotly_chart(fig, use_container_width=True)
 
