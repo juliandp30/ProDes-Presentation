9a4b0b75d5dfb9d0484a5a46557d6e310ae91319
juliandp30 <j.parra@rdc-consultores.com>
2023-05-16 11:42:52 -0500

    dticks

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ if submitted1:
     )
     fig.update_layout(font_size=20, plot_bgcolor='rgba(180, 180, 180, 0.3)')
     fig.update_xaxes(title="Opciones de refuerzo", visible=True, showticklabels=False)
-    fig.update_yaxes(title="Precio", visible=True, showticklabels=True, dtick=5)
+    fig.update_yaxes(title="Precio", visible=True, showticklabels=True, dtick=5000000)
 
     st.plotly_chart(fig, use_container_width=True)
 
@@ -100,7 +100,7 @@ if submitted1:
         height=700,
     )
     fig.update_layout(font_size=20, plot_bgcolor='rgba(180, 180, 180, 0.3)')
-    fig.update_xaxes(title="Precio", visible=True, showticklabels=True)
+    fig.update_xaxes(title="Precio", visible=True, showticklabels=True, dtick=5000000)
     fig.update_yaxes(title="Puntaje", visible=True, showticklabels=True, dtick=0.25)
 
     st.plotly_chart(fig, use_container_width=True)
@@ -129,7 +129,7 @@ if submitted1:
     )
     fig.update_layout(font_size=20, plot_bgcolor='rgba(180, 180, 180, 0.3)')
     fig.update_xaxes(title="Opciones de refuerzo", visible=True, showticklabels=False)
-    fig.update_yaxes(title="# Piezas", visible=True, showticklabels=True, dtick=5)
+    fig.update_yaxes(title="# Piezas", visible=True, showticklabels=True, dtick=2000)
 
     st.plotly_chart(fig, use_container_width=True)
 
