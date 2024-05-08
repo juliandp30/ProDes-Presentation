322b2e4988152ab0daece518e3e51a3b0602607e
juliandp30 <j.parra@rdc-consultores.com>
2024-05-08 10:25:46 -0500

    yrange

--- main.py
+++ main.py
@@ -109,11 +109,7 @@ if submitted1:
         y=list_gh["by_weigth"],
         height=700,
     )
-    fig.update_layout(
-        font_size=20,
-        plot_bgcolor="rgba(180, 180, 180, 0.3)",
-        layout_yaxis_range=[0, max(list_gh["by_weigth"]) * 2],
-    )
+    fig.update_layout(font_size=20, plot_bgcolor="rgba(180, 180, 180, 0.3)")
     fig.update_xaxes(title="Opciones de refuerzo", visible=True, showticklabels=False)
     fig.update_yaxes(title="Peso total", visible=True, showticklabels=True, dtick=5)
 
@@ -127,6 +123,7 @@ if submitted1:
         height=700,
     )
     fig.update_layout(font_size=20, plot_bgcolor="rgba(180, 180, 180, 0.3)")
+    fig.update_layout(layout_yaxis_range=[0, max(list_gh["by_unitweigth"])*2])
     fig.update_xaxes(title="Opciones de refuerzo", visible=True, showticklabels=False)
     fig.update_yaxes(title="Tenor", visible=True, showticklabels=False)
 
