4ee2204289f1c08f35a881d625d7d4a7fc07e62e
juliandp30 <j.parra@rdc-consultores.com>
2024-05-08 10:32:46 -0500

    no more colocacion de piezas

--- main.py
+++ main.py
@@ -136,18 +136,18 @@ if submitted1:
 
     st.plotly_chart(fig, use_container_width=True)
 
-    st.title("Colocación de piezas de refuerzo (# Operaciones)")
+    # st.title("Colocación de piezas de refuerzo (# Operaciones)")
 
-    fig = px.scatter(
-        x=list_gh["keys"],
-        y=list_gh["by_pieces"],
-        height=700,
-    )
-    fig.update_layout(font_size=20, plot_bgcolor="rgba(180, 180, 180, 0.3)")
-    fig.update_xaxes(title="Opciones de refuerzo", visible=True, showticklabels=False)
-    fig.update_yaxes(title="# Piezas", visible=True, showticklabels=True, dtick=500)
+    # fig = px.scatter(
+    #     x=list_gh["keys"],
+    #     y=list_gh["by_pieces"],
+    #     height=700,
+    # )
+    # fig.update_layout(font_size=20, plot_bgcolor="rgba(180, 180, 180, 0.3)")
+    # fig.update_xaxes(title="Opciones de refuerzo", visible=True, showticklabels=False)
+    # fig.update_yaxes(title="# Piezas", visible=True, showticklabels=True, dtick=500)
 
-    st.plotly_chart(fig, use_container_width=True)
+    # st.plotly_chart(fig, use_container_width=True)
 
     st.title("Gestión del inventario de la obra")
 
