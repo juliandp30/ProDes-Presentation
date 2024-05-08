ec51bb701cea6a6487912106bd5b2e85117b74a8
juliandp30 <j.parra@rdc-consultores.com>
2024-05-08 10:35:39 -0500

    textos

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ with st.sidebar.form(key="Form1"):
 
     scores_data = dict(
         by_weigth=st.number_input("Puntaje por Peso", value=5, min_value=1),
-        by_figures=st.number_input("Puntaje por #Figuras", value=5, min_value=1),
+        by_figures=st.number_input("Puntaje por #Tipologías", value=5, min_value=1),
         by_pieces=st.number_input("Puntaje por #Operaciones", value=5, min_value=1),
-        by_blueprints=st.number_input("Puntaje por #Planos", value=5, min_value=1),
+        by_blueprints=st.number_input("Puntaje por simplicidad", value=5, min_value=1),
     )
 
     st.header("Precios de barras (COP)")
@@ -149,7 +149,7 @@ if submitted1:
 
     # st.plotly_chart(fig, use_container_width=True)
 
-    st.title("Gestión del inventario de la obra")
+    st.title("Número de tipologías de barras")
 
     fig = px.scatter(
         x=list_gh["keys"],
@@ -158,7 +158,7 @@ if submitted1:
     )
     fig.update_layout(font_size=20, plot_bgcolor="rgba(180, 180, 180, 0.3)")
     fig.update_xaxes(title="Opciones de refuerzo", visible=True, showticklabels=False)
-    fig.update_yaxes(title="# Figuras", visible=True, showticklabels=True, dtick=10)
+    fig.update_yaxes(title="# Tipologías", visible=True, showticklabels=True, dtick=10)
 
     st.plotly_chart(fig, use_container_width=True)
 
